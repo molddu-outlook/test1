7d863f2b481c8f5b2ce698370463eaacf66f8c68
김영기 (AI Robot서비스팀) <youngki.kim@kt.com>
2025-04-07 15:22:57 +0900

    Initial commit

---
+++ t.py
@@ -0,0 +1,22 @@
+#test python code
+import os
+import sys
+import time
+import random
+import string
+import math
+import re
+import json
+import urllib
+import urllib.request
+import urllib.parse
+import urllib.error
+import urllib.robotparser
+
+
+# test python code
+def test():
+    print("Hello, World!")
+    print("This is a test
+    print("This is a test")
+    print("This is a test")   
